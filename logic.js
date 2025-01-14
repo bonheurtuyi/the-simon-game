08f6f5dfd04b9a965f744e31804b2203c1a97e8f
[Bonheur Tuyikunde] <[bonheurtuyikunde2@gmail.com]>
2025-01-14 16:06:38 +0200

    [Check which button is pressed]

--- logic.js
+++ logic.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     let gamePattern = [];
 
+    let userClickedPattern = [];
+
     let buttonColors = ["red", "blue", "green", "yellow" ];
 
     function nextSequence(){
@@ -9,11 +11,16 @@ $(document).ready(function () {
         gamePattern.push(randomChosenColor);
 
         const button = $("#" + randomChosenColor);
-        button.fadeOut(100).fadeIn(100);
+        button.fadeIn(100).fadeOut(100).fadeIn(100);
 
-        button.click(() =>{
-            let audio = new Audio("./sounds/" + randomChosenColor + ".mp3");
-            audio.play();
+        //This code no longer works as the browser requires user interaction to play any sound
+        let audio = new Audio("./sounds/" + randomChosenColor + ".mp3");
+        audio.play();
+
+        $(".btn").click(function () {
+            let userChosenColor = $(this).attr("id");
+            userClickedPattern.push(userChosenColor);
+            console.log(userClickedPattern);
         })
     }
 
