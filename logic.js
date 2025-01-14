806f7eca708ef83cdb12bcbb8b8cdf34316c044b
[Bonheur Tuyikunde] <[bonheurtuyikunde2@gmail.com]>
2025-01-14 17:07:06 +0200

    [Start the game]

--- logic.js
+++ logic.js
@@ -1,28 +1,41 @@
 $(document).ready(function () {
     let gamePattern = [];
-
+    let level = 0;
     let userClickedPattern = [];
-
+    let started = false;
     let buttonColors = ["red", "blue", "green", "yellow" ];
 
+    //Detect keyboard press and checking if the game has already started
+    $(document).keydown(function (event) {
+        if (!started){
+            $("#level-title").text("Level " + level);
+            nextSequence();
+            started = true;
+        }
+    })
+
+    $(".btn").click(function () {
+        let userChosenColor = $(this).attr("id");
+        userClickedPattern.push(userChosenColor);
+
+        playSound(userChosenColor);
+        animatePress(userChosenColor);
+    })
+
     function nextSequence(){
+        level++;
+
+        $("#level-title").text("Level " + level);
+
         let randomNumber = Math.floor(Math.random()*4);
         let randomChosenColor = buttonColors[randomNumber];
         gamePattern.push(randomChosenColor);
 
-        const button = $("#" + randomChosenColor);
-        button.fadeIn(100).fadeOut(100).fadeIn(100);
+        $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
         //This code no longer works as the browser requires user interaction to play any sound
         playSound(randomChosenColor);
 
-        $(".btn").click(function () {
-            let userChosenColor = $(this).attr("id");
-            userClickedPattern.push(userChosenColor);
-            console.log(userClickedPattern);
-            playSound(userChosenColor);
-            animatePress(userChosenColor);
-        })
     }
 
     //Function to play sound
@@ -41,5 +54,4 @@ $(document).ready(function () {
         }, 100);
     }
 
-    nextSequence();
 })
